42e57c06cf32deec2604b653a4e8012a68731c48
Abd3lrhm4n <abdelrhmanelsaid681@gmail.com>
2019-02-04 11:53:31 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace PngChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<FileInfo> FinishedImages = new List<FileInfo>();
+            List<FileInfo> PureImages = new List<FileInfo>();
+
+            List<FileInfo> NotFinishedImages = new List<FileInfo>();
+
+            Regex PathValidate = new Regex("^([a-zA-Z]:)?(\\\\[^<>:\"/\\\\|?*]+)+\\\\?$");
+            if (args.Count() == 0)
+            {
+                System.Console.WriteLine("PngChecker [PNG Directory] [RAW Directory]");
+                return;
+            }
+            else if(args.Count() > 2)
+            {
+                System.Console.WriteLine("There're much arguments");
+                return;
+            }
+
+            //check if the path wrote in right syntax
+            if (!PathValidate.IsMatch(args[0]) && !PathValidate.IsMatch(args[1]))
+            {
+                System.Console.WriteLine("Please Check the Directory Path Syntax");
+                return;
+            }
+
+            var PureDirectory = new DirectoryInfo(args[1]);
+
+            var FinishedDicretory = new DirectoryInfo(args[0]);
+
+            //check if the directories are exsit
+            if (!PureDirectory.Exists || !FinishedDicretory.Exists)
+            {
+                System.Console.WriteLine("There's one or both of Directory not Exist");
+                return;
+            }
+            var FinishedFiles = FinishedDicretory.GetFiles();
+            var Purefiles = PureDirectory.GetFiles();
+
+            //add pure images into list
+            foreach (var file in Purefiles)
+            {
+                PureImages.Add(file);
+            }
+
+            //add finished images into list
+            foreach (var file in FinishedFiles)
+            {                
+                FinishedImages.Add(file);   
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            System.Console.WriteLine("Checking...");
+
+            for (int i = 0; i < PureImages.Count(); i++)
+            {
+                for (int j = 0; j < FinishedImages.Count(); j++)
+                {
+                    //get the image name without the Extension
+                    string FinishedName = FinishedImages[j].Name
+                    .Replace(FinishedImages[j].Name
+                    .Substring(FinishedImages[j].Name.IndexOf(FinishedImages[j].Extension, FinishedImages[j].Extension.Length)), "");
+
+                    //get the image name without the Extension
+                    string PureName = PureImages[i].Name
+                    .Replace(PureImages[i].Name
+                    .Substring(PureImages[i].Name.IndexOf(PureImages[i].Extension, PureImages[i].Extension.Length)), "");
+
+                    if(FinishedName == PureName)
+                    {
+                        break;
+                    }
+                    else if(FinishedName != PureName && j == FinishedImages.Count() - 1)
+                    {
+                        NotFinishedImages.Add(PureImages[i]);
+
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        System.Console.WriteLine(PureImages[i]);
+                    }
+                }
+                                    
+            
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            System.Console.WriteLine($"{NotFinishedImages.Count()} Images are Missing");
+
+            if (NotFinishedImages.Count() > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                System.Console.WriteLine("******************************************");
+                Console.ForegroundColor = ConsoleColor.Red;
+                System.Console.WriteLine("Missing Files");
+
+                foreach (var item in NotFinishedImages)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    System.Console.WriteLine(item);
+                }
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                System.Console.WriteLine("******************************************");
+
+            }
+            
+            
+            string targetPath = FinishedDicretory + "\\Missing\\";
+
+            if (!Directory.Exists(targetPath))
+            {
+                Directory.CreateDirectory(targetPath);
+            }
+
+            foreach (var item in NotFinishedImages)
+            {
+                File.Copy(item.ToString(), targetPath + item.Name, true);
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+#Png-Checker
+
+That Application made for some people who working with photoshop
+they were converting alot of RAW images to PNG so they wanted to get the RAW images which not converted to PNG
+
+
+#USAGE 
+In CMD >PngChecker ["PNG Directory Path"] ["RAW Directory Path"] 
+
+#NOTICE 
+both folder Images must contain the same images names
+
+#Such as 
+IMG_341.JPG must be IMG_341.PNG if not the application will consider the IMG_341.JPG is missing because the application working on the name of images or files
+
+#After all 
+the application will create a directory its name is missing and will copy all missing images into it
